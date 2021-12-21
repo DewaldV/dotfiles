f124c47485c7fe9efb4a0ab99af914e0bdc5dedc
DewaldV <dewald.viljoen@uswitch.com>
2021-12-21 21:01:49 +0000

    Doom: Add default config and a few more tools/lang

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Dewald Viljoen"
+      user-mail-address "dewald.viljoen@rvu.co.uk")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -29,8 +29,9 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/GDrive/Documents/notes/")
-(setq projectile-project-search-path '("~/Projects/"))
+(setq
+ org-directory "~/GDrive/Documents/notes/"
+ projectile-project-search-path '("~/Projects/"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
