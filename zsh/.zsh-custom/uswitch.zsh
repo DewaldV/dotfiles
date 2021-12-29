453523f39ee70b043d1c59195aa59c138e1ae5ac
DewaldV <dewald.viljoen@uswitch.com>
2021-12-29 20:01:02 +0000

    zsh: Remove old watson and go commands

--- zsh/.zsh-custom/uswitch.zsh
+++ zsh/.zsh-custom/uswitch.zsh
@@ -1,27 +1,2 @@
 ## From https://github.com/uswitch/u
 if which u &> /dev/null; then eval "$(u --completion-script-zsh)"; fi
-
-func golnusw() {
-  local project=${1}
-  ln -s ~/Projects/${project} ${GOPATH}/src/github.com/uswitch/${project}
-}
-
-func godev() {
-  cd ${GOPATH}/src/github.com/${1}
-}
-
-func waval() {
-  watson start value $@
-}
-
-func watoil() {
-  watson start toil $@
-}
-
-func waover() {
-  watson start overhead $@
-}
-
-func wastop() {
-  watson stop
-}
