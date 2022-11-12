13c44f6a95762ef3cae7f16e1cff1a619d055c16
DewaldV <dewald.viljoen@pm.me>
2022-11-12 08:19:26 +0000

    emacs: move org notes dir to Dropbox

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Documents/GDrive/notes/")
+(setq org-directory "~/Documents/Dropbox/notes/")
 (setq projectile-project-search-path '("~/Projects/"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
